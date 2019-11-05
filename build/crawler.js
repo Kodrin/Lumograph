487b5e71b43983eedf6103c58f62329b875acd51
Codrin-Mihail <kodrintxps@gmail.com>
2019-11-05 13:55:03 +0900

    file name is now delimited correctly
    + started test environment
    + started txt saving
    + moved things around

--- build/crawler.js
+++ build/crawler.js
@@ -10,34 +10,38 @@ function Crawler(directory)
     const fs = require("fs");
     const path = require('path');
 
+    //directories
+    const databasePath = "./build/database/";
+    const rollFile = "roll.txt";
+
     let files = fs.readdirSync(directory); //read the directory
-    let fileList = [];
+    let rollList = [];
     let indexCounter = 0;
 
-    // this.crawl = function ()
-    // {
-        for (const file in files) {
-            if (fs.statSync(directory + files[file]).isDirectory()) 
-            {
-                fileList = Crawler(directory + files[file] + '/', fileList); //continue recursion
-            }
-            else 
-            {
-                var src = directory + files[file]; //source of input files[file]
-                var slicedExt = files[file].slice(0, -4); //remove extension
-                var dst = __dirname + "/public/collections/"+ indexCounter + ".jpg"; //source of destination
-                var pathName = path.basename(directory); //directory name
-                var rollIndex = pathName.slice(0,2);
-                var roll = pathName.slice(3,-4); //gets the name of the roll from directory
-                var isometric = pathName.slice(-3); //gets the iso value of the roll
-        
-                console.log(src);
-        
-                indexCounter ++;
-            }
-            
+    for (const file in files) {
+        if (fs.statSync(directory + files[file]).isDirectory()) 
+        {
+            //THE NEW WAY (SPLITTING!!)
+            /*****
+             * [0] is the roll number
+             * [1] is the roll type
+             * [2] is the roll iso
+             * [3] is the roll size (35mm, 120mm)
+             *****
+                */
+            const src = directory + files[file];
+            const delimiter = "%";
+            let unfiltered = src.replace(directory, "");
+            let filtered = unfiltered.split(delimiter);
+            rollList.push(filtered);
+
+            console.log("Roll Nbr. " + filtered[0] + " ");
+            console.log(filtered);
         }
-    // }
+    }
+
+
+    fs.writeFileSync(databasePath + rollFile, rollList);
     
 
 }
--- index.js
+++ index.js
@@ -2,8 +2,28 @@ console.time('Application time')
 console.log("This is the lumograph initialize")
 
 const Crawler = require('./build/crawler')
-const crawler = new Crawler("./archive/");
+const crawler = new Crawler("./archive/rolls/");
 // crawler.crawl();
+// const fs = require("fs");
+// for (let i = 0; i < 166; i++) {
+//     const directory = "./archive/rolls/";
+//     let index = i;
+//     let rollType = ["Ektar","Fuji","Washi-S"];
+//     let iso = [50,200,400,800,1600];
+//     let rollSize = [35,120,200];
+
+//     let dirName = 
+//         index + "%" +
+//         rollType[Math.floor(Math.random()*rollType.length)] + "%" +
+//         iso[Math.floor(Math.random() * iso.length)] + "%" +
+//         rollSize[Math.floor(Math.random() * rollSize.length)]
+//     ;
+
+//     fs.mkdirSync(
+//         directory + dirName
+//     );
+    
+// }
 
 
 console.timeEnd('Application time')
