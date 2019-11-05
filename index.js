bc5ff609260fa73280d240958247d8a94b0d1fb9
Codrin-Mihail <kodrintxps@gmail.com>
2019-11-05 12:49:57 +0900

    minor changes

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ console.log("This is the lumograph initialize")
 
 const Crawler = require('./build/crawler')
 const crawler = new Crawler("./archive/");
+// crawler.crawl();
 
 
 console.timeEnd('Application time')
