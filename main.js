a858b8b6096d08732d44cfdb705a3e2613c3a04f
Codrin-Mihail <kodrintxps@gmail.com>
2019-11-12 00:52:31 +0900

    started index.html for electron

--- main.js
+++ main.js
@@ -3,9 +3,13 @@ const { app, BrowserWindow } = require('electron')
 function createWindow () {
   // Create the browser window.
   let win = new BrowserWindow({
-    width: 800,
+    width: 1200,
     height: 600,
     webPreferences: {
+      // skipTaskbar: true,
+      // frame: false,
+      skipTaskbar: process.platform === 'darwin',
+      autoHideMenuBar: process.platform === 'darwin',
       nodeIntegration: true
     }
   })
